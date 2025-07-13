71aad68e225b965831540b81b05b6f40166e3f9f
bri <284789+b-@users.noreply.github.com>
2025-07-13 14:35:40 -0400

    fix path

--- build-sea.sh
+++ build-sea.sh
@@ -10,4 +10,3 @@ node --experimental-sea-config sea-config.json
 cp $(command -v node) sshpk-conv
 npx postject sshpk-conv NODE_SEA_BLOB sea-prep.blob \
     --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
-cp sshpk-conv /work/
